fecfc78cca61b3a682e8139408f623f30c37d7cc
klimalen <alenaksenova@gmail.com>
2026-03-29 22:59:32 +0300

    Delete js directory

--- js/config.js
+++
@@ -1,7 +0,0 @@
-(function(){
-  'use strict';
-  window.APP_VERSION = '1.26.5';
-  window.ymGoal = function(id){
-    try{ if(window.ym) window.ym(108255731,'reachGoal',id); }catch(e){}
-  };
-})();
