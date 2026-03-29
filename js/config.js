715092e1d938ea9d320720928fb232c89e6ef23c
klimalen <alenaksenova@gmail.com>
2026-03-29 20:52:38 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 (function(){
   'use strict';
-  window.APP_VERSION = '1.0.4';
+  window.APP_VERSION = '1.26.5';
   window.ymGoal = function(id){
     try{ if(window.ym) window.ym(108255731,'reachGoal',id); }catch(e){}
   };
