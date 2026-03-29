799378492e23a7e0b66a44a7e836c2c7c27d3f59
klimalen <alenaksenova@gmail.com>
2026-03-29 22:55:08 +0300

    folder logic

---
+++ config.js
@@ -0,0 +1,7 @@
+(function(){
+  'use strict';
+  window.APP_VERSION = '1.26.5';
+  window.ymGoal = function(id){
+    try{ if(window.ym) window.ym(108255731,'reachGoal',id); }catch(e){}
+  };
+})();
